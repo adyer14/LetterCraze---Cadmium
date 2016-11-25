85f88190d72b712bf60c7a0ec94ef072fcbe2a04
Juan Pablo de Lima B. <jdelima@wpi.edu>
2016-11-24 23:50:38 -0500

    # WARNING: head commit changed in the meantime
    Created ExitLevelController and NewLevelController cwith constructors

---
+++ LetterCraze - Cadmium/src/builder/controller/ExitLevelController.java
@@ -0,0 +1,27 @@
+package builder.controller;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import builder.view.*;
+import builder.model.*;
+
+public class ExitLevelController extends MouseAdapter {
+	Level level;
+	LevelCreatorPanel levelCreatorView;
+	MainMenu mainMenu;
+	MainMenuPanel mainMenuView;
+	
+	public ExitLevelController(Level lvl, LevelCreatorPanel lcv, MainMenu mm, MainMenuPanel mmv){
+		this.level = lvl;
+		this.levelCreatorView = lcv;
+		this.mainMenu = mm;
+		this.mainMenuView = mmv;
+	}
+	
+	@Override
+	public void MousePressed(MouseEvent me){
+		//impl
+	}
+
+}
---
+++ LetterCraze - Cadmium/src/builder/controller/NewLevelController.java
@@ -0,0 +1,23 @@
+package builder.controller;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class NewLevelController extends MouseAdapter {
+	Level level;
+	LevelCreatorPanel levelCreatorView;
+	MainMenu mainMenu;
+	MainMenuPanel mainMenuView;
+	
+	public NewLevelController(Level lvl, LevelCreatorPanel lcv, MainMenu mm, MainMenuPanel mmv){
+		this.level = lvl;
+		this.levelCreatorView = lcv;
+		this.mainMenu = mm;
+		this.mainMenuView = mmv;
+	}
+	
+	@Override
+	public void MousePressed(MouseEvent me){
+		//impl
+	}
+}
