1edcf6620d6c54d01bee1ccbfa0bdf66d444e75c
Monika <medanielewicz@wpi.edu>
2016-11-27 01:16:48 -0500

    Created BuilderApplication and set up splash screen

---
+++ LetterCraze - Cadmium/src/builder/view/BuilderApplication.java
@@ -0,0 +1,58 @@
+package builder.view;
+
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
+public class BuilderApplication extends JFrame {
+	
+	/**
+	 * Keep Eclipse happy
+	 */
+	private static final long serialVersionUID = 7707913344008839015L;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		SplashScreenWindow splashWindow = new SplashScreenWindow();
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					splashWindow.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		try {
+		    Thread.sleep(1000);
+		} catch (InterruptedException e) {
+		    e.printStackTrace();
+		}
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					splashWindow.setVisible(false);
+					BuilderApplication frame = new BuilderApplication();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public BuilderApplication() {
+		super();
+
+		setBounds(250, 80, 816, 589);
+		setTitle("Builder");
+		
+
+	}
+
+}
