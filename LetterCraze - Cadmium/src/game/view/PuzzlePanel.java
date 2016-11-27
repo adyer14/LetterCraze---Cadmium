86c3edbe58736b7bf82011518317be356b902daa
Monika <medanielewicz@wpi.edu>
2016-11-27 00:51:56 -0500

    Added serialID

--- LetterCraze - Cadmium/src/game/view/PuzzlePanel.java
+++ LetterCraze - Cadmium/src/game/view/PuzzlePanel.java
@@ -3,6 +3,11 @@ package game.view;
 public class PuzzlePanel extends LevelPanel {
 
 	/**
+	 * Keep Eclipse happy
+	 */
+	private static final long serialVersionUID = -2099720612660676023L;
+
+	/**
 	 * Create the panel.
 	 */
 	public PuzzlePanel() {
