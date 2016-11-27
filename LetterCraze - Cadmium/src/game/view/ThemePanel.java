abaea1d23fc7f30a31176ba6978c25ab23de759c
Monika <medanielewicz@wpi.edu>
2016-11-27 00:51:29 -0500

    Added serialID and a TODO

--- LetterCraze - Cadmium/src/game/view/ThemePanel.java
+++ LetterCraze - Cadmium/src/game/view/ThemePanel.java
@@ -1,7 +1,12 @@
 package game.view;
 
 public class ThemePanel extends LevelPanel {
+	/**
+	 * Keep Eclipse happy
+	 */
+	private static final long serialVersionUID = 6784567307261293313L;
 
+	// TODO add in theme level name 
 	/**
 	 * Create the panel.
 	 */
