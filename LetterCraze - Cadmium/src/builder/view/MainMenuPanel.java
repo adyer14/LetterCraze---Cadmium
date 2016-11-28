ecf708762f9ce0c589674a2d3e78e66758b2048c
Monika <medanielewicz@wpi.edu>
2016-11-28 00:01:01 -0500

    Developed MainMenuPanel

--- LetterCraze - Cadmium/src/builder/view/MainMenuPanel.java
+++ LetterCraze - Cadmium/src/builder/view/MainMenuPanel.java
@@ -1,6 +1,13 @@
 package builder.view;
 
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.border.LineBorder;
 
 public class MainMenuPanel extends JPanel {
 
@@ -13,7 +20,46 @@ public class MainMenuPanel extends JPanel {
 	 * Create the panel.
 	 */
 	public MainMenuPanel() {
+		setBounds(0, 0, 800, 550);
+		setBackground(new Color(230, 230, 250));
+		setLayout(null);
+		
+		JPanel titlePanel = new JPanel();
+		titlePanel.setBounds(0, 0, 800, 160);
+		titlePanel.setBackground(new Color(119, 136, 153));
+		titlePanel.setLayout(null);
+		add(titlePanel);
+		
+		JLabel mainMenuLabel = new JLabel("Main Menu");
+		mainMenuLabel.setForeground(Color.BLACK);
+		mainMenuLabel.setHorizontalAlignment(SwingConstants.CENTER);	
+		mainMenuLabel.setFont(new Font("OCR A Extended", Font.BOLD, 70));
+		mainMenuLabel.setBounds(0, 0, 800, 160);
+		titlePanel.add(mainMenuLabel);
 
+		JButton newLevelButton = new JButton("NEW LEVEL");
+		newLevelButton.setForeground(new Color(0, 0, 0));
+		newLevelButton.setBackground(new Color(176, 196, 222));
+		newLevelButton.setFont(new Font("OCR A Extended", Font.BOLD, 30));
+		newLevelButton.setBounds(267, 210, 280, 70);
+		newLevelButton.setBorder(new LineBorder(new Color(119, 136, 153), 3));
+		add(newLevelButton);
+		
+		JButton loadLevelButton = new JButton("LOAD LEVEL");
+		loadLevelButton.setForeground(new Color(0, 0, 0));
+		loadLevelButton.setFont(new Font("OCR A Extended", Font.BOLD, 30));
+		loadLevelButton.setBackground(new Color(176, 196, 222));
+		loadLevelButton.setBounds(267, 320, 280, 70);
+		loadLevelButton.setBorder(new LineBorder(new Color(119, 136, 153), 3));
+		add(loadLevelButton);
+		
+		JButton deleteLevelButton = new JButton("DELETE LEVEL");
+		deleteLevelButton.setForeground(new Color(0, 0, 0));
+		deleteLevelButton.setFont(new Font("OCR A Extended", Font.BOLD, 30));
+		deleteLevelButton.setBackground(new Color(176, 196, 222));
+		deleteLevelButton.setBounds(267, 430, 280, 70);
+		deleteLevelButton.setBorder(new LineBorder(new Color(119, 136, 153), 3));
+		add(deleteLevelButton);
 	}
 
 }
