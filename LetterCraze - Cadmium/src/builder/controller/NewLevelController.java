fd9cb804e564960115a324cf4da8cee81c4b2508
Monika <medanielewicz@wpi.edu>
2016-11-30 17:41:29 -0500

    Changed NewLevelController to an ActionListener

--- LetterCraze - Cadmium/src/builder/controller/NewLevelController.java
+++ LetterCraze - Cadmium/src/builder/controller/NewLevelController.java
@@ -1,30 +1,41 @@
 package builder.controller;
 
+import java.awt.CardLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+
+import javax.swing.JPanel;
 
 import builder.view.*;
 //import builder.model.*;
 
-public class NewLevelController extends MouseAdapter {
+public class NewLevelController implements ActionListener {
+	//BuilderApplication bApp;
 	//Level level;
-	LevelCreatorPanel levelCreatorView;
+	//LevelCreatorPanel levelCreatorView;
 	//MainMenu mainMenu;
-	MainMenuPanel mainMenuView;
+	MainMenuPanel mmv;
 	
-	public NewLevelController(/*Level lvl,*/ LevelCreatorPanel lcv, /*MainMenu mm,*/ MainMenuPanel mmv){
+	public NewLevelController(/*Level lvl, LevelCreatorPanel lcv, MainMenu mm,*/ MainMenuPanel mainMenuPanel){
 		//this.level = lvl;
-		this.levelCreatorView = lcv;
+		//this.levelCreatorView = lcv;
 		//this.mainMenu = mm;
-		this.mainMenuView = mmv;
+		this.mmv = mainMenuPanel;
+		//this.bApp = bApp;
 	}
 	
-	public void MouseClicked(MouseEvent me){
 	
-		mainMenuView.setVisible(false);
+	public void actionPerformed(ActionEvent e) {
+		JPanel contentPane = mmv.getContentPane();
+            CardLayout cardLayout = (CardLayout) contentPane.getLayout();
+            cardLayout.next(contentPane);
+ 
+		//mainMenuView.setVisible(false);
 		//mainMenuView.invalidate();
 		
-		levelCreatorView.setVisible(true);
+		//bApp.getLevelCreatorPanel().setVisible(true);
 		//levelCreatorView.validate();
 	}
 }
