be1927c4cde2f563f62847fef8edb7c23ef3ec5b
Monika <medanielewicz@wpi.edu>
2016-11-30 17:41:09 -0500

    Changed ExitLevelController to an ActionListener

--- LetterCraze - Cadmium/src/builder/controller/ExitLevelController.java
+++ LetterCraze - Cadmium/src/builder/controller/ExitLevelController.java
@@ -1,31 +1,34 @@
 package builder.controller;
 
+import java.awt.CardLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+
+import javax.swing.JPanel;
 
 import builder.view.*;
 //import builder.model.*;
 
-public class ExitLevelController extends MouseAdapter {
+public class ExitLevelController  implements ActionListener {
 	//Level level;
-	LevelCreatorPanel levelCreatorView;
+	LevelCreatorPanel lcp;
 	//MainMenu mainMenu;
-	MainMenuPanel mainMenuView;
+	//MainMenuPanel mainMenuView;
 	
-	public ExitLevelController(/*Level lvl,*/ LevelCreatorPanel lcv, /*MainMenu mm,*/ MainMenuPanel mmv){
+	public ExitLevelController(/*Level lvl,*/ LevelCreatorPanel levelCreatorPanel){
 		//this.level = lvl; 
-		this.levelCreatorView = lcv;
+		this.lcp = levelCreatorPanel;
 		//this.mainMenu = mm;
-		this.mainMenuView = mmv;
+		//this.mainMenuView = mmv;
 	}
 	
-	public void MousePressed(MouseEvent me){
-		
-		levelCreatorView.setVisible(false);
-		//levelCreatorView.invalidate();
-		
-		mainMenuView.setVisible(true);
-		//mainMenuView.validate();
+	public void actionPerformed(ActionEvent e) {
+		JPanel contentPane = lcp.getContentPane();
+            CardLayout cardLayout = (CardLayout) contentPane.getLayout();
+            cardLayout.next(contentPane);
+
 	}
 
 }
