d5adc2d02bef33547c84a9f22ede92578980ecaa
Alex <awd1467@gmail.com>
2016-11-30 13:55:08 -0500

    Fixing Main

--- LetterCraze - Cadmium/src/game/LaunchLetterCraze.java
+++ LetterCraze - Cadmium/src/game/LaunchLetterCraze.java
@@ -2,41 +2,16 @@ package game;
 
 import java.awt.EventQueue;
 
+import javax.swing.JFrame;
+
+import builder.view.BuilderApplication;
 import builder.view.SplashScreenWindow;
 import game.view.LCApplication;
 
 public class LaunchLetterCraze {
-		
 	
-	public static void main(String[] args) {
-		SplashScreenWindow splashWindow = new SplashScreenWindow();
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					splashWindow.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-		try {
-		    Thread.sleep(1000);
-		} catch (InterruptedException e) {
-		    e.printStackTrace();
-		}
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					splashWindow.setVisible(false);
-					LCApplication frame = new LCApplication();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-		
-	}
-
+	LCApplication app = new LCApplication();
+	app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	app.setVisible(true);
 
 }
