4b18ec2fd7d3aac03a1c04aed8b07b7f88c2b9b0
Juan Pablo de Lima B. <jdelima@wpi.edu>
2016-11-30 13:58:21 -0500

    Created buttons as fields for MainMenuPanel

--- LetterCraze - Cadmium/src/builder/view/MainMenuPanel.java
+++ LetterCraze - Cadmium/src/builder/view/MainMenuPanel.java
@@ -2,6 +2,8 @@ package builder.view;
 
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
@@ -15,6 +17,10 @@ public class MainMenuPanel extends JPanel {
 	 * Keep Eclipse happy
 	 */
 	private static final long serialVersionUID = 6797683652563023292L;
+	
+	JButton newLevelButton;
+	JButton loadLevelButton;
+	JButton deleteLevelButton;
 
 	/**
 	 * Create the panel.
@@ -37,7 +43,7 @@ public class MainMenuPanel extends JPanel {
 		mainMenuLabel.setBounds(0, 0, 800, 160);
 		titlePanel.add(mainMenuLabel);
 
-		JButton newLevelButton = new JButton("NEW LEVEL");
+		newLevelButton = new JButton("NEW LEVEL");
 		newLevelButton.setForeground(new Color(0, 0, 0));
 		newLevelButton.setBackground(new Color(176, 196, 222));
 		newLevelButton.setFont(new Font("OCR A Extended", Font.BOLD, 30));
@@ -45,7 +51,7 @@ public class MainMenuPanel extends JPanel {
 		newLevelButton.setBorder(new LineBorder(new Color(119, 136, 153), 3));
 		add(newLevelButton);
 		
-		JButton loadLevelButton = new JButton("LOAD LEVEL");
+		loadLevelButton = new JButton("LOAD LEVEL");
 		loadLevelButton.setForeground(new Color(0, 0, 0));
 		loadLevelButton.setFont(new Font("OCR A Extended", Font.BOLD, 30));
 		loadLevelButton.setBackground(new Color(176, 196, 222));
@@ -53,7 +59,7 @@ public class MainMenuPanel extends JPanel {
 		loadLevelButton.setBorder(new LineBorder(new Color(119, 136, 153), 3));
 		add(loadLevelButton);
 		
-		JButton deleteLevelButton = new JButton("DELETE LEVEL");
+		deleteLevelButton = new JButton("DELETE LEVEL");
 		deleteLevelButton.setForeground(new Color(0, 0, 0));
 		deleteLevelButton.setFont(new Font("OCR A Extended", Font.BOLD, 30));
 		deleteLevelButton.setBackground(new Color(176, 196, 222));
