40f54d341b3ebd96a1d85ed10de32c2f458af79d
Alex <awd1467@gmail.com>
2016-11-30 17:25:31 -0500

    Edited board Panel Constructor

--- LetterCraze - Cadmium/src/game/view/BoardPanel.java
+++ LetterCraze - Cadmium/src/game/view/BoardPanel.java
@@ -2,12 +2,16 @@ package game.view;
 
 import javax.swing.JPanel;
 
+import game.model.Model;
+
 public class BoardPanel extends JPanel {
 
+	Model m;
 	/**
 	 * Create the panel.
 	 */
-	public BoardPanel() {
+	public BoardPanel(Model model) {
+		this.m = model;
 
 	}
 
