6ca3da7ffd775b88a90c30491fac0a44f5472b12
Monika <medanielewicz@wpi.edu>
2016-11-30 18:12:46 -0500

    Changed to actionListener, commented out code for later

--- LetterCraze - Cadmium/src/game/controller/ExitLevelController.java
+++ LetterCraze - Cadmium/src/game/controller/ExitLevelController.java
@@ -1,32 +1,30 @@
 package game.controller;
 
+import java.awt.CardLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
+import javax.swing.JPanel;
+
 import game.model.Level;
 import game.view.LevelPanel;
-import game.view.LevelSelectPanel;
 
-public class ExitLevelController extends MouseAdapter {
-	Level level;
-	LevelPanel levelView;
-	LevelSelectPanel levelSelectView;
+public class ExitLevelController  implements ActionListener {
+	Level lvl;
+	LevelPanel lp;
 	
-	public ExitLevelController(Level lvl, LevelPanel lcv, LevelSelectPanel mmv){
-		this.level = lvl; 
-		this.levelView = lcv;
-		this.levelSelectView = mmv;
+	public ExitLevelController(Level level, LevelPanel levelPanel){
+		this.lvl = level; 
+		this.lp = levelPanel;
 	}
 	
-	public void MousePressed(MouseEvent me){
-		
-		//save progress
-		
-		levelView.setVisible(false);
-		//levelCreatorView.invalidate();
-		
-		levelSelectView.setVisible(true);
-		//mainMenuView.validate();
+	public void actionPerformed(ActionEvent e) {
+		JPanel contentPane = lp.getContentPane();
+        CardLayout cardLayout = (CardLayout) contentPane.getLayout();
+        cardLayout.next(contentPane);
+
 	}
 
 }
