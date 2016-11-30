eda70f39ec3c1c8356d289f49056751c95e0a620
Monika <medanielewicz@wpi.edu>
2016-11-30 13:42:01 -0500

    Fixed launcher

--- LetterCraze - Cadmium/src/builder/view/BuilderApplication.java
+++ LetterCraze - Cadmium/src/builder/view/BuilderApplication.java
@@ -12,39 +12,6 @@ public class BuilderApplication extends JFrame {
 
 	private MainMenuPanel mmp;
 	private LevelCreatorPanel lcp;
-	
-	/**
-	 * Launch the application.
-	 */
-	/*public static void main(String[] args) {
-		SplashScreenWindow splashWindow = new SplashScreenWindow();
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					splashWindow.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-		try {
-		    Thread.sleep(100);
-		} catch (InterruptedException e) {
-		    e.printStackTrace();
-		}
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					splashWindow.setVisible(false);
-					BuilderApplication frame = new BuilderApplication();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-		
-	}*/
 
 	/**
 	 * Create the frame.
