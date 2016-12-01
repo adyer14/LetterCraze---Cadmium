9669334cb8a56b2b8413adf531cbc4bd144b6900
Monika <medanielewicz@wpi.edu>
2016-11-30 19:55:50 -0500

    Created TilePanel

--- LetterCraze - Cadmium/src/game/view/TilePanel.java
+++ LetterCraze - Cadmium/src/game/view/TilePanel.java
@@ -1,14 +1,29 @@
 package game.view;
 
+import java.awt.Color;
+
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
 public class TilePanel extends JPanel {
 
 	/**
+	 * Keep Eclipse happy
+	 */
+	private static final long serialVersionUID = -872998560741757223L;
+
+	/**
 	 * Create the panel.
 	 */
-	public TilePanel() {
-
+	public TilePanel(Color color) {
+		setBounds(0, 0, 40, 40);
+		setBackground(color);
+		setLayout(null);
+		
+		JButton tileButton = new JButton();
+		tileButton.setBounds(5, 5, 30, 30);
+		tileButton.setBackground(color);
+		add(tileButton);
+		
 	}
-
 }
