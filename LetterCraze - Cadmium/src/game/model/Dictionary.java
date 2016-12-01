e604d0bdb8ff25e8fc8dca1cdc9fa70c523b22ee
Monika <medanielewicz@wpi.edu>
2016-12-01 02:45:00 -0500

    Added get/set, changed dSize to 3 so it'd work but it's a HACK

--- LetterCraze - Cadmium/src/game/model/Dictionary.java
+++ LetterCraze - Cadmium/src/game/model/Dictionary.java
@@ -2,17 +2,25 @@ package game.model;
 
 public class Dictionary {
 	
-	int dSize = 100;
-	String words [] = new String [dSize];
+	private int dSize = 3;
+	String words [] = new String [getdSize()];
 	
 	public Dictionary (String dictionary []) {
-		for (int i = 0; i < dSize; i++) {
+		for (int i = 0; i < getdSize(); i++) {
 			words [i] = dictionary [i];
 		}
 	}
 	
 	public boolean containsWord (String word) {
 		return false;
+	}
+
+	public int getdSize() {
+		return dSize;
+	}
+
+	public void setdSize(int dSize) {
+		this.dSize = dSize;
 	}
 
 }
