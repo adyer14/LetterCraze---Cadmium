219aae8b839b04d4d962fa3d5c71b3e1ba21fa5e
Monika <medanielewicz@wpi.edu>
2016-11-30 19:38:55 -0500

    Added get/set methods

--- LetterCraze - Cadmium/src/game/model/Model.java
+++ LetterCraze - Cadmium/src/game/model/Model.java
@@ -2,7 +2,14 @@ package game.model;
 
 public class Model {
 	
-	Level level [] = new Level [15];
-	LevelSelect ls = new LevelSelect (level);
+	private Level level [] = new Level [15];
+	LevelSelect ls = new LevelSelect (getLevel());
+	
+	public Level[] getLevel() {
+		return level;
+	}
+	public void setLevel(Level level[]) {
+		this.level = level;
+	}
 	
 }
