9d2fc6f1cfa076f229b0305b95dd8734bac896f0
Monika <medanielewicz@wpi.edu>
2016-11-30 19:37:40 -0500

    Filled in boardPanel

--- LetterCraze - Cadmium/src/game/view/BoardPanel.java
+++ LetterCraze - Cadmium/src/game/view/BoardPanel.java
@@ -1,18 +1,30 @@
 package game.view;
 
-import javax.swing.JPanel;
+import java.awt.Color;
 
+import javax.swing.JPanel;
+import javax.swing.border.LineBorder;
+import game.model.Level;
 import game.model.Model;
 
 public class BoardPanel extends JPanel {
 
+	/**
+	 * Keep Eclipse happy
+	 */
+	private static final long serialVersionUID = 854408960237025508L;
+	
 	Model m;
 	/**
 	 * Create the panel.
 	 */
 	public BoardPanel(Model model) {
 		this.m = model;
-
+		setBounds(0, 0, 254, 254);
+		setBackground(new Color(119, 136, 153));
+		setLayout(null);
+		setBorder(new LineBorder(new Color(176, 196, 222), 6));
+		
 	}
 
 }
