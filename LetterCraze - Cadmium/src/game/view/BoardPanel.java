2d3981a588b7f103537185b046f1a0b7ecb564f0
Monika <medanielewicz@wpi.edu>
2016-12-01 02:44:21 -0500

    Made BoardPanel prettier

--- LetterCraze - Cadmium/src/game/view/BoardPanel.java
+++ LetterCraze - Cadmium/src/game/view/BoardPanel.java
@@ -2,11 +2,7 @@ package game.view;
 
 import java.awt.Color;
 
-import javax.swing.ImageIcon;
 import javax.swing.JPanel;
-import javax.swing.JToggleButton;
-import javax.swing.border.LineBorder;
-import game.model.Level;
 import game.model.Model;
 
 public class BoardPanel extends JPanel {
@@ -18,29 +14,29 @@ public class BoardPanel extends JPanel {
 	
 	private TilePanel tilePanel[][] = new TilePanel[6][6];
 	Model m;
+	
 	/**
 	 * Create the panel.
 	 */
 	public BoardPanel(Model model) {
 		this.m = model;
 		setBounds(0, 0, 254, 254);
-		setBackground(new Color(119, 136, 153));
+		setBackground(new Color(176, 196, 222));
 		setLayout(null);
-		setBorder(new LineBorder(new Color(176, 196, 222), 6));
-		
+	
 		Color darkColor = new Color(216, 191, 216);
 		Color lightColor = new Color(255, 228, 225);
 		
+		// Creates the TilePanels with alternating colors
 		boolean dark = true;
 		for (int i = 0; i <= 5; i++) {
 			dark = !dark;
 			for (int j = 0; j <= 5; j++) {
 				dark = !dark;
-				
 				if (dark)
-					tilePanel[i][j] = new TilePanel(darkColor);
+					tilePanel[i][j] = new TilePanel(model, darkColor);
 				else
-					tilePanel[i][j] = new TilePanel(lightColor);
+					tilePanel[i][j] = new TilePanel(model, lightColor);
 				tilePanel[i][j].setBounds((j*40) + 7, (i*40) + 7, 40, 40);
 
 				add(tilePanel[i][j]);
