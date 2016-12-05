dba7d2edee766e9db2557c3f45c38c91b9372ceb
Monika <medanielewicz@wpi.edu>
2016-12-04 23:22:26 -0500

    Added more method parameters and two methods
    Added Level and LevelSelectPanel as constructor parameters
    Added updateStars and unlockNext method calls

--- LetterCraze - Cadmium/src/game/controller/ExitLevelController.java
+++ LetterCraze - Cadmium/src/game/controller/ExitLevelController.java
@@ -4,23 +4,32 @@ import java.awt.CardLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JPanel;
+import game.model.Level;
 import game.model.Model;
 import game.view.LevelPanel;
+import game.view.LevelSelectPanel;
 
 public class ExitLevelController  implements ActionListener {
 	Model m;
 	LevelPanel lp;
+	Level level;
+	LevelSelectPanel lsp;
 	
-	public ExitLevelController(Model model, LevelPanel levelPanel){
+	public ExitLevelController(Model model, Level level, LevelPanel levelPanel, LevelSelectPanel lsp){
 		this.m = model; 
+		this.level = level;
 		this.lp = levelPanel;
+		this.lsp = lsp;
 	}
 	
 	public void actionPerformed(ActionEvent e) {
 		JPanel contentPane = lp.getContentPane();
         CardLayout cardLayout = (CardLayout) contentPane.getLayout();
         cardLayout.show(contentPane, "levelSelectPanel");
-
+        
+        lsp.updateStars(level);
+        if ((level.getMostStars() > 0) && (level.getLevelNumber() < 5)) 
+        	lsp.unlockNext(level);
 	}
 
 }
