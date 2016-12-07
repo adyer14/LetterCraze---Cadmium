ac680e140c4d01691610ef02da7a247cc241ecc6
Juan Pablo de Lima B. <jdelima@wpi.edu>
2016-12-07 18:13:06 -0500

    Created DeleteLevelController constructor

--- LetterCraze - Cadmium/src/builder/controller/DeleteLevelController.java
+++ LetterCraze - Cadmium/src/builder/controller/DeleteLevelController.java
@@ -1,5 +1,17 @@
 package builder.controller;
 
-public class DeleteLevelController {
+import builder.model.*;
+import builder.view.*;
 
+public class DeleteLevelController {
+	Level lvl;
+	MainMenu mainMenu;
+	MainMenuPanel mainMenuPanel;
+	
+	public DeleteLevelController(Level lvl, MainMenu mainMenu, MainMenuPanel mainMenuPanel){
+		this.lvl = lvl;
+		this.mainMenu = mainMenu;
+		this.mainMenuPanel = mainMenuPanel;
+	}
+	
 }
