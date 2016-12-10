3ff8ca6a1344fa3c3ee458b6f13a2055e2613335
Monika <medanielewicz@wpi.edu>
2016-12-09 19:16:18 -0500

    Fixed tilePanel representation, added resetTiles method

--- LetterCraze - Cadmium/src/game/view/BoardPanel.java
+++ LetterCraze - Cadmium/src/game/view/BoardPanel.java
@@ -3,7 +3,11 @@ package game.view;
 import java.awt.Color;
 
 import javax.swing.JPanel;
+
+import game.controller.ChooseWordController;
+import game.model.Board;
 import game.model.Model;
+import game.model.Tile;
 
 public class BoardPanel extends JPanel {
 
@@ -12,37 +16,60 @@ public class BoardPanel extends JPanel {
 	 */
 	private static final long serialVersionUID = 854408960237025508L;
 	
-	private TilePanel tilePanel[][] = new TilePanel[6][6];
+	private TilePanel tilePanel[] = new TilePanel[36];
 	Model m;
+	Board board;
 	
 	/**
 	 * Create the panel.
 	 */
-	public BoardPanel(Model model) {
+	public BoardPanel(Model model, Board b) {
 		this.m = model;
+		this.board = b;
+		
 		setBounds(0, 0, 254, 254);
 		setBackground(new Color(176, 196, 222));
 		setLayout(null);
+		
+		initTiles();
+	}
 	
+	public void initTiles() {
+		
+		// create controller for choosing words
+		ChooseWordController CWcontrol = new ChooseWordController(this);
+
 		Color darkColor = new Color(216, 191, 216);
 		Color lightColor = new Color(255, 228, 225);
-		
-		// Creates the TilePanels with alternating colors
 		boolean dark = true;
-		for (int i = 0; i <= 5; i++) {
-			dark = !dark;
-			for (int j = 0; j <= 5; j++) {
-				dark = !dark;
-				if (dark)
-					tilePanel[i][j] = new TilePanel(model, darkColor);
-				else
-					tilePanel[i][j] = new TilePanel(model, lightColor);
-				tilePanel[i][j].setBounds((j*40) + 7, (i*40) + 7, 40, 40);
-
-				add(tilePanel[i][j]);
-			}
-		}
+		int row, col;
 		
+		// create and add the TilePanels with alternating colors
+		for (int i = 0; i < 36; i++) {
+			Tile tile = board.getBoardSquareByIndex(i).getTile();
+			
+			row = (int) Math.floor(i/6);
+			col = i%6;
+			if (col == 0)
+					dark = !dark;
+			if (dark) {
+				tilePanel[i] = new TilePanel(tile, darkColor, CWcontrol);
+				dark = !dark;
+			}
+			else {		
+				tilePanel[i] = new TilePanel(tile, lightColor, CWcontrol);
+				dark = !dark;
+			}
+			
+			tilePanel[i].setBounds((col*40) + 7, (row*40) + 7, 40, 40);
+			add(tilePanel[i]);
+		}
+	}
+	
+	public void resetTiles() {
+		for (int i = 0; i < 36; i++) {
+			tilePanel[i].resetColors();
+		}
 	}
 
 }
