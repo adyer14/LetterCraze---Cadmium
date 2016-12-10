1dda2cbbca9b8848ec2f0b368983dadb4add8361
Monika <medanielewicz@wpi.edu>
2016-12-09 19:25:25 -0500

    Cleaned up, added parameters, got mouse things working
    Removed some unnecessary parameters like LevelPanel and Dictionary
    Decided that this controller is going to be created by BoardPanel
    Developed mousePressed, mouseEntered, and mouseReleased methods to show
    user feedback in GUI; not linked with entities yet

--- LetterCraze - Cadmium/src/game/controller/ChooseWordController.java
+++ LetterCraze - Cadmium/src/game/controller/ChooseWordController.java
@@ -1,56 +1,66 @@
 package game.controller;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-
-import game.model.Dictionary;
+import javax.swing.JButton;
 import game.model.Level;
-import game.view.LevelPanel;
+import game.view.BoardPanel;
+import game.view.TilePanel;
 
 public class ChooseWordController implements MouseListener {
 
 	Level level;
-	LevelPanel levPanel;
-	Dictionary dict;
+	BoardPanel bp;
+	private boolean isWordBeingCreated = false;
 	
-	public ChooseWordController(Level l, LevelPanel lp, Dictionary dict){
-		this.level = l;
-		this.levPanel = lp;
-		this.dict = dict;
+	public ChooseWordController(/*Level l, */BoardPanel bp){
+		//this.level = l;
+		this.bp = bp;
 	}
-
-
-	@Override
-	public void mouseClicked(MouseEvent arg0) {
-		// TODO Auto-generated method stub
 	
-	}
-
-	@Override
-	public void mouseEntered(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-		//if mousePressed flag set add letter to word
-	}
-
-	@Override
-	public void mouseExited(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-		//none
-	}
-
 	@Override
 	public void mousePressed(MouseEvent arg0) {
-		// TODO Auto-generated method stub
 		// set flag, can start capturing values of tiles
+		isWordBeingCreated = true;
+		
+		// update graphics
+		JButton selectedButton = (JButton) arg0.getSource();
+		TilePanel tp = (TilePanel)selectedButton.getParent();
+		tp.setSelected();
+	}
+	
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		//if mousePressed flag set add letter to word
+		
+		// update graphics
+		JButton selectedButton = (JButton) arg0.getSource();
+		TilePanel tp = (TilePanel)selectedButton.getParent();
+		if (isWordBeingCreated) {
+			tp.setSelected();
+		}
 	}
 
 	@Override
 	public void mouseReleased(MouseEvent arg0) {
-		// TODO Auto-generated method stub
 		//finish word hand off to check word
+		isWordBeingCreated = false;
+		
+		// reset graphics
+		bp.resetTiles();	
 	}
 	
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// keep the button selected after exiting container
+		
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
 	
 }
