a3146678009d75c1e75b250af668a47999b53208
Monika <medanielewicz@wpi.edu>
2016-12-09 19:21:54 -0500

    Added Entity references, controller init, methods; made it pretty
    Connected button to the single ChooseWordController passed from Board
    Added resetColors and setSelected methods for choosing a word

--- LetterCraze - Cadmium/src/game/view/TilePanel.java
+++ LetterCraze - Cadmium/src/game/view/TilePanel.java
@@ -1,11 +1,12 @@
 package game.view;
 
 import java.awt.Color;
-
 import javax.swing.JButton;
 import javax.swing.JPanel;
-
-import game.model.Model;
+import game.controller.ChooseWordController;
+import game.model.LetterTile;
+import game.model.Tile;
+import java.awt.Font;
 
 public class TilePanel extends JPanel {
 
@@ -14,21 +15,49 @@ public class TilePanel extends JPanel {
 	 */
 	private static final long serialVersionUID = -872998560741757223L;
 
-	private Model m;
+	private Tile tile;
+	private JButton tileButton;
+	private String letter;
+	private Color unselectedColor;
+	private Color letterSelected = new Color(51, 0, 0);
+	private Color selectedColor = new Color(255, 153, 153);
 	
 	/**
 	 * Create the panel.
 	 */
-	public TilePanel(Model model, Color color) {
-		this.m = model;
+	public TilePanel(Tile t, Color color, ChooseWordController controller) {
+		this.tile = t;
+		this.unselectedColor = color;
+		if (tile instanceof LetterTile) {
+			this.letter = ((LetterTile)tile).getLetter();
+		}
+		
 		setBounds(0, 0, 40, 40);
-		setBackground(color);
+		setBackground(unselectedColor);
 		setLayout(null);
 		
-		JButton tileButton = new JButton();
+		tileButton = new JButton();
+		tileButton.setText(letter);
+		tileButton.setFont(new Font("OCR A Extended", Font.PLAIN, 30));
 		tileButton.setBounds(5, 5, 30, 30);
 		tileButton.setBackground(color);
-		add(tileButton);
+		tileButton.setBorder(null);
+		tileButton.setFocusPainted(false);
+		tileButton.setContentAreaFilled(false);
+		tileButton.addMouseListener(controller);
 		
+		add(tileButton);
+	}
+	
+	public void setSelected() {
+		tileButton.setBackground(selectedColor);
+		this.setBackground(selectedColor);
+		tileButton.setForeground(letterSelected);
+	}
+	
+	public void resetColors() {
+		tileButton.setBackground(unselectedColor);
+		this.setBackground(unselectedColor);
+		tileButton.setForeground(null);
 	}
 }
