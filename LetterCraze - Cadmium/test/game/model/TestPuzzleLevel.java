2a345d59db3293955008c4abb25cb57287d9c1cd
Monika <medanielewicz@wpi.edu>
2016-12-13 14:30:17 -0500

    Added more testing

--- LetterCraze - Cadmium/test/game/model/TestPuzzleLevel.java
+++ LetterCraze - Cadmium/test/game/model/TestPuzzleLevel.java
@@ -90,22 +90,18 @@ public class TestPuzzleLevel extends TestCase {
 		int score = lvl.removeScore();
 		assertEquals(score,0);
 		
-		lvl.addScore(w1);
 		lvl.addWord(w1);
-		lvl.addScore(w2);
 		lvl.addWord(w2);
-		score = lvl.removeScore();
 		lvl.removeWord();
-		assertEquals (score, w1.calculateScore());
+		assertEquals (lvl.getScore(), w1.calculateScore());
 		
 		score = lvl.removeScore();
-		assertEquals (score, 0);
+		assertEquals (lvl.getScore(), 0);
 	}
 	
 	public void testCheckStarProgress() {
 		assertEquals(lvl.checkStarProgress(), 0);
 		
-		lvl.addScore(w3);
 		lvl.addWord(w3);
 		assertEquals(lvl.checkStarProgress(), 0);
 		assertEquals(lvl.getHighScore(), lvl.getScore());
@@ -142,14 +138,140 @@ public class TestPuzzleLevel extends TestCase {
 	}
 	
 	public void testRandomLetter () {
+		int numAs = 0;
+		int numBs = 0;
+		int numCs = 0;
+		int numDs = 0;
 		int numEs = 0;
-		for (int i=0; i < 1000; i++) {
-			String isItAnE = lvl.randomLetter();
-			if (isItAnE == "A") {
+		int numFs = 0;
+		int numGs = 0;
+		int numHs = 0;
+		int numIs = 0;
+		int numJs = 0;
+		int numKs = 0;
+		int numLs = 0;
+		int numMs = 0;
+		int numNs = 0;
+		int numOs = 0;
+		int numPs = 0;
+		int numQs = 0;
+		int numRs = 0;
+		int numSs = 0;
+		int numTs = 0;
+		int numUs = 0;
+		int numVs = 0;
+		int numWs = 0;
+		int numXs = 0;
+		int numYs = 0;
+		int numZs = 0;
+		for (int i=0; i < 10000; i++) {
+			String randLetter = ((LetterTile)lvl.randomLetter()).getLetter();
+			if (randLetter == "A") {
+				numAs++;
+			}
+			if (randLetter == "B") {
+				numBs++;
+			}
+			if (randLetter == "C") {
+				numCs++;
+			}
+			if (randLetter == "D") {
+				numDs++;
+			}
+			if (randLetter == "E") {
 				numEs++;
 			}
+			if (randLetter == "F") {
+				numFs++;
+			}
+			if (randLetter == "G") {
+				numGs++;
+			}
+			if (randLetter == "H") {
+				numHs++;
+			}
+			if (randLetter == "I") {
+				numIs++;
+			}
+			if (randLetter == "J") {
+				numJs++;
+			}
+			if (randLetter == "K") {
+				numKs++;
+			}
+			if (randLetter == "L") {
+				numLs++;
+			}
+			if (randLetter == "M") {
+				numMs++;
+			}
+			if (randLetter == "N") {
+				numNs++;
+			}
+			if (randLetter == "O") {
+				numOs++;
+			}
+			if (randLetter == "P") {
+				numPs++;
+			}
+			if (randLetter == "Q") {
+				numQs++;
+			}
+			if (randLetter == "R") {
+				numRs++;
+			}
+			if (randLetter == "S") {
+				numSs++;
+			}
+			if (randLetter == "T") {
+				numTs++;
+			}
+			if (randLetter == "U") {
+				numUs++;
+			}
+			if (randLetter == "V") {
+				numVs++;
+			}
+			if (randLetter == "W") {
+				numWs++;
+			}
+			if (randLetter == "X") {
+				numXs++;
+			}
+			if (randLetter == "Y") {
+				numYs++;
+			}
+			if (randLetter == "Z") {
+				numZs++;
+			}
+		
 		}
 		System.out.println("" + numEs);
+		System.out.println("" + numTs);
+		System.out.println("" + numAs);
+		System.out.println("" + numOs);
+		System.out.println("" + numIs);
+		System.out.println("" + numNs);
+		System.out.println("" + numSs);
+		System.out.println("" + numHs);
+		System.out.println("" + numRs);
+		System.out.println("" + numDs);
+		System.out.println("" + numLs);
+		System.out.println("" + numCs);
+		System.out.println("" + numUs);
+		System.out.println("" + numMs);
+		System.out.println("" + numWs);
+		System.out.println("" + numFs);
+		System.out.println("" + numGs);
+		System.out.println("" + numYs);
+		System.out.println("" + numPs);
+		System.out.println("" + numBs);
+		System.out.println("" + numVs);
+		System.out.println("" + numKs);
+		System.out.println("" + numJs);
+		System.out.println("" + numXs);
+		System.out.println("" + numQs);
+		System.out.println("" + numZs);
 	}
 	
 	
