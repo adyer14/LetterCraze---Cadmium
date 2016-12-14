b72f771762d6ed3a9261ab5a8e6ed40df65e38d4
Monika <medanielewicz@wpi.edu>
2016-12-13 19:24:25 -0500

    Fixed broken loop, added getSpecificBoardSquare method, fixed calls

--- LetterCraze - Cadmium/src/game/model/Board.java
+++ LetterCraze - Cadmium/src/game/model/Board.java
@@ -12,8 +12,8 @@ public class Board {
 	
 	public Board () {
 		int k = 0;
-		for (int i = 0; i < 5; i++) {
-			for (int j = 0; j < 5; j++) {
+		for (int i = 0; i <= 5; i++) {
+			for (int j = 0; j <= 5; j++) {
 			boardSquares [k] = new Square (i, j, true, blankTile);
 			k++;
 			}
@@ -53,17 +53,17 @@ public class Board {
 	
 	public boolean floatTilesUp () {
 		for (int i = 0; i < 36; i++) {
-			if (boardSquares[i].squareInPlay && 
-					boardSquares[i].tile == null) {
-				for (int j = boardSquares[i].squareColumn; j < 36; j++) {
-					if (boardSquares[j].squareInPlay &&
-							boardSquares[j].tile == null &&
-							boardSquares[j].squareColumn == boardSquares[i].squareColumn) {
-						for (int k = (boardSquares[j].squareColumn + 6*boardSquares[j].squareRow); k < 36; k++) {
-							if (boardSquares[k].squareInPlay &&
-									boardSquares[k].tile != null &&
-									boardSquares[k].squareColumn == boardSquares[j].squareColumn) {
-								boardSquares[j].tile = boardSquares[k].tile;
+			if (boardSquares[i].getSquareInPlay() && 
+					boardSquares[i].getTile() == null) {
+				for (int j = boardSquares[i].getSquareColumn(); j < 36; j++) {
+					if (boardSquares[j].getSquareInPlay() &&
+							boardSquares[j].getTile() == null &&
+							boardSquares[j].getSquareColumn() == boardSquares[i].getSquareColumn()) {
+						for (int k = (boardSquares[j].getSquareColumn() + 6*boardSquares[j].getSquareRow()); k < 36; k++) {
+							if (boardSquares[k].getSquareInPlay() &&
+									boardSquares[k].getTile() != null &&
+									boardSquares[k].getSquareColumn() == boardSquares[j].getSquareColumn()) {
+								boardSquares[j].setTile(boardSquares[k].getTile());
 								boardSquares[k].removeTile();
 							}
 						}
@@ -82,6 +82,10 @@ public class Board {
 		}
 	}
 	
+	public Square getSpecificBoardSquare(int index) {
+		return boardSquares[index];
+	}
+	
 	/**
 	 * Get/set methods
 	 */
@@ -92,7 +96,7 @@ public class Board {
 	public void setBoardSquares(Square boardSquares[]) {
 		this.boardSquares = boardSquares;
 	}
-
+	
 	public HashMap<String, Integer> getLetterScores() {
 		return letterScores;
 	}
