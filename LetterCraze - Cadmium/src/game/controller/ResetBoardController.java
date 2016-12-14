78d19ec70ee971189119cbf187fa2be6b82ce903
Monika <medanielewicz@wpi.edu>
2016-12-14 04:32:21 -0500

    Finished resetBoardController

--- LetterCraze - Cadmium/src/game/controller/ResetBoardController.java
+++ LetterCraze - Cadmium/src/game/controller/ResetBoardController.java
@@ -2,24 +2,28 @@ package game.controller;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
-
 import game.model.Level;
+import game.model.Model;
 import game.view.LevelPanel;
 
 public class ResetBoardController implements ActionListener{
 	Level level;
 	LevelPanel levPanel;
+	Model m;
+	private String levType;
+	private int levNum;
 
-public ResetBoardController(Level l, LevelPanel lp) {
-	this.level = l;
-	this.levPanel = lp;
-}
+	public ResetBoardController(Model m, String levType, int levNum, LevelPanel lp) {
+		this.m = m;
+		this.levType = levType;
+		this.levNum = levNum;
+		this.levPanel = lp;
+	}
 
-
-@Override
-public void actionPerformed(ActionEvent arg0) {
-	// TODO Auto-generated method stub
-	
-}
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		this.level = this.m.getSpecificLevel(levType, levNum);
+		level.resetLevel();
+		levPanel.refresh();
+	}
 }
