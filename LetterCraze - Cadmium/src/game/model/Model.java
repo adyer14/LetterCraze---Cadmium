289f0cd22a246f68477f26028c562097cd041cc1
Monika <medanielewicz@wpi.edu>
2016-12-18 04:34:37 -0500

    Clean up, clean up, everybody do your share

--- LetterCraze - Cadmium/src/game/model/Model.java
+++ LetterCraze - Cadmium/src/game/model/Model.java
@@ -18,266 +18,33 @@ public class Model {
 	PuzzleLevel puzzleLevel[] = new PuzzleLevel[6];
 	LightningLevel lightningLevel[] = new LightningLevel[6];
 	ThemeLevel themeLevel[] = new ThemeLevel[6];
-	LevelSelect ls = new LevelSelect (getLevels());
 	ArrayList<Square> randomBoardSquares = new ArrayList<Square>(36);
 	ArrayList<Square> foodSquares = new ArrayList<Square>();
 	ArrayList<Square> rSquares = new ArrayList<Square>();
 	ArrayList<Square> fourSquares = new ArrayList<Square>();
 	ArrayList<Square> duneSquares = new ArrayList<Square>();
 	ArrayList<Square> latinSquares = new ArrayList<Square>();
-	
-	
-	// THESE ARE TEST VARIABLES, DELETE WHEN WE GET FILE UPLOAD WORKING
 		private int[] starVal;
-		//Board board = new Board(null);
 		private int numMoves;
 		private int time;
-		//String list [] = {"yes", "no", "maybe"};
-		//List<String> themeWords = new ArrayList<String>(Arrays.asList(list));
-		//ThemeDictionary themeDic = new ThemeDictionary(themeWords);
-		//String themeName = "Example";
-		
-		String foodList [] = {"PIE", "BURGER", "PIZZA"};
-		List<String> foodWords = new ArrayList<String>(Arrays.asList(foodList));
-		ThemeDictionary foodDic = new ThemeDictionary(foodWords);
-		String foodName = "Food";
-		
-		String rList [] = {"RAW", "RAT", "RED", "RIP"};
-		List<String> rWords = new ArrayList<String>(Arrays.asList(rList));
-		ThemeDictionary rDic = new ThemeDictionary(rWords);
-		String rName = "R";
-		
-		String fourList [] = {"FOUR", "FOUR", "FOUR", "Four"};
-		List<String> fourWords = new ArrayList<String>(Arrays.asList(fourList));
-		ThemeDictionary fourDic = new ThemeDictionary(fourWords);
-		String fourName = "Four";
-		
-		String duneList [] = {"MELANGE", "ARRAKIS", "MUADDIB"};
-		List<String> duneWords = new ArrayList<String>(Arrays.asList(duneList));
-		ThemeDictionary duneDic = new ThemeDictionary(fourWords);
-		String duneName = "Dune";
-	/**
-	 * Constructor
-	 */
-		String latinList [] = {"CATULLUS", "CAESAR", "HORACE"};
-		List<String> latinWords = new ArrayList<String>(Arrays.asList(latinList));
-		ThemeDictionary latinDic = new ThemeDictionary(latinWords);
-		String latinName = "Latin Bros";
 		private Board board;
 		private String squaresInPlay;
 		private String themeName;
 		private String themeWords;
 		
-		
+	/**
+	 * Constructor
+	 */
 	public Model() {
 		board = new Board(null);
 		randomBoardSquares = new ArrayList<Square>(36);
 		ArrayList<Tile> initialTiles = new ArrayList<Tile>();
 		
-		// Generate Theme Letters
-		foodSquares.add(new Square(0, 0, true, randomTile()));//0
-		foodSquares.add(new Square(0, 1, true, new LetterTile("P", 4)));//1
-		foodSquares.add(new Square(0, 2, true, new LetterTile("I", 2)));//2
-		foodSquares.add(new Square(0, 3, true, randomTile()));//3
-		foodSquares.add(new Square(0, 4, true, randomTile()));//4
-		foodSquares.add(new Square(0, 5, true, randomTile()));//5
-		foodSquares.add(new Square(1, 0, true, randomTile()));//6
-		foodSquares.add(new Square(1, 1, true, randomTile()));//7
-		foodSquares.add(new Square(1, 2, true, new LetterTile("E", 1)));//8
-		foodSquares.add(new Square(1, 3, true, randomTile()));//9
-		foodSquares.add(new Square(1, 4, true, randomTile()));//10
-		foodSquares.add(new Square(1, 5, true, randomTile()));//11
-		foodSquares.add(new Square(2, 0, true, randomTile()));//12
-		foodSquares.add(new Square(2, 1, true, randomTile()));//13
-		foodSquares.add(new Square(2, 2, true, randomTile()));//14
-		foodSquares.add(new Square(2, 3, true, randomTile()));//15
-		foodSquares.add(new Square(2, 4, true, randomTile()));//16
-		foodSquares.add(new Square(2, 5, true, randomTile()));//17
-		foodSquares.add(new Square(3, 0, true, randomTile()));//18
-		foodSquares.add(new Square(3, 1, true, randomTile()));//19
-		foodSquares.add(new Square(3, 2, true, new LetterTile("B", 4)));//20
-		foodSquares.add(new Square(3, 3, true, new LetterTile("U", 3)));//21
-		foodSquares.add(new Square(3, 4, true, new LetterTile("R", 2)));//22
-		foodSquares.add(new Square(3, 5, true, new LetterTile("G", 4)));//23
-		foodSquares.add(new Square(4, 0, true, randomTile()));//24
-		foodSquares.add(new Square(4, 1, true, randomTile()));//25
-		foodSquares.add(new Square(4, 2, true, randomTile()));//26
-		foodSquares.add(new Square(4, 3, true, randomTile()));//27
-		foodSquares.add(new Square(4, 4, true, new LetterTile("R", 2)));//28
-		foodSquares.add(new Square(4, 5, true, new LetterTile("E", 1)));//29
-		foodSquares.add(new Square(5, 0, true, new LetterTile("P", 4)));//30
-		foodSquares.add(new Square(5, 1, true, new LetterTile("I", 2)));//31
-		foodSquares.add(new Square(5, 2, true, new LetterTile("Z", 8)));//32
-		foodSquares.add(new Square(5, 3, true, new LetterTile("Z", 8)));//33
-		foodSquares.add(new Square(5, 4, true, new LetterTile("A", 2)));//34
-		foodSquares.add(new Square(5, 5, true, randomTile()));//35
-		
-		rSquares.add(new Square(0, 0, true, new LetterTile("R", 2)));//0
-		rSquares.add(new Square(0, 1, true, new LetterTile("E", 1)));//1
-		rSquares.add(new Square(0, 2, true, new LetterTile("D", 3)));//2
-		rSquares.add(new Square(0, 3, false,new BlankTile()));//3
-		rSquares.add(new Square(0, 4, false,new BlankTile()));//4
-		rSquares.add(new Square(0, 5, false,new BlankTile()));//5
-		rSquares.add(new Square(1, 0, true, randomTile()));//6
-		rSquares.add(new Square(1, 1, false,new BlankTile()));//7
-		rSquares.add(new Square(1, 2, true, randomTile()));//8
-		rSquares.add(new Square(1, 3, false,new BlankTile()));//9
-		rSquares.add(new Square(1, 4, false,new BlankTile()));//10
-		rSquares.add(new Square(1, 5, false,new BlankTile()));//11
-		rSquares.add(new Square(2, 0, true, new LetterTile("R", 2)));//12
-		rSquares.add(new Square(2, 1, true, new LetterTile("A", 2)));//13
-		rSquares.add(new Square(2, 2, true, new LetterTile("W", 3)));//14
-		rSquares.add(new Square(2, 3, false,new BlankTile()));//15
-		rSquares.add(new Square(2, 4, false,new BlankTile()));//16
-		rSquares.add(new Square(2, 5, false,new BlankTile()));//17
-		rSquares.add(new Square(3, 0, true, randomTile()));//18
-		rSquares.add(new Square(3, 1, true, new LetterTile("R", 2)));//19
-		rSquares.add(new Square(3, 2, false,new BlankTile()));//20
-		rSquares.add(new Square(3, 3, true, new LetterTile("R", 2)));//21
-		rSquares.add(new Square(3, 4, false,new BlankTile()));//22
-		rSquares.add(new Square(3, 5, false,new BlankTile()));//23
-		rSquares.add(new Square(4, 0, true, new LetterTile("A", 2)));//24
-		rSquares.add(new Square(4, 1, true, randomTile()));//25
-		rSquares.add(new Square(4, 2, false,new BlankTile()));//26
-		rSquares.add(new Square(4, 3, false,new BlankTile()));//27
-		rSquares.add(new Square(4, 4, true, new LetterTile("I", 2)));//28
-		rSquares.add(new Square(4, 5, false,new BlankTile()));//29
-		rSquares.add(new Square(5, 0, true, new LetterTile("T", 1)));//30
-		rSquares.add(new Square(5, 1, true, randomTile()));//31
-		rSquares.add(new Square(5, 2, false,new BlankTile()));//32
-		rSquares.add(new Square(5, 3, false,new BlankTile()));//33
-		rSquares.add(new Square(5, 4, false,new BlankTile()));//34
-		rSquares.add(new Square(5, 5, true, new LetterTile("P", 4)));//35
-		
-		fourSquares.add(new Square(0, 0, true, new LetterTile("F", 4)));//0
-		fourSquares.add(new Square(0, 1, true, new LetterTile("O", 2)));//1
-		fourSquares.add(new Square(0, 2, true, randomTile()));//2
-		fourSquares.add(new Square(0, 3, true, randomTile()));//3
-		fourSquares.add(new Square(0, 4, true, new LetterTile("F", 4)));//4
-		fourSquares.add(new Square(0, 5, true, new LetterTile("O", 2)));//5
-		fourSquares.add(new Square(1, 0, true, new LetterTile("U", 3)));//6
-		fourSquares.add(new Square(1, 1, true, new LetterTile("R", 2)));//7
-		fourSquares.add(new Square(1, 2, true, randomTile()));//8
-		fourSquares.add(new Square(1, 3, true, randomTile()));//9
-		fourSquares.add(new Square(1, 4, true, new LetterTile("U", 3)));//10
-		fourSquares.add(new Square(1, 5, true, new LetterTile("R", 2)));//11
-		fourSquares.add(new Square(2, 0, true, randomTile()));//12
-		fourSquares.add(new Square(2, 1, true, randomTile()));//13
-		fourSquares.add(new Square(2, 2, false,new BlankTile()));//14
-		fourSquares.add(new Square(2, 3, false,new BlankTile()));//15
-		fourSquares.add(new Square(2, 4, true, randomTile()));//16
-		fourSquares.add(new Square(2, 5, true, randomTile()));//17
-		fourSquares.add(new Square(3, 0, true, randomTile()));//18
-		fourSquares.add(new Square(3, 1, true, randomTile()));//19
-		fourSquares.add(new Square(3, 2, false,new BlankTile()));//20
-		fourSquares.add(new Square(3, 3, false,new BlankTile()));//21
-		fourSquares.add(new Square(3, 4, true, randomTile()));//22
-		fourSquares.add(new Square(3, 5, true, randomTile()));//23
-		fourSquares.add(new Square(4, 0, true, new LetterTile("F", 4)));//24
-		fourSquares.add(new Square(4, 1, true, new LetterTile("O", 2)));//25
-		fourSquares.add(new Square(4, 2, true, randomTile()));//26
-		fourSquares.add(new Square(4, 3, true, randomTile()));//27
-		fourSquares.add(new Square(4, 4, true, new LetterTile("F", 4)));//28
-		fourSquares.add(new Square(4, 5, true, new LetterTile("O", 2)));//29
-		fourSquares.add(new Square(5, 0, true, new LetterTile("U", 3)));//30
-		fourSquares.add(new Square(5, 1, true, new LetterTile("R", 2)));//31
-		fourSquares.add(new Square(5, 2, true, randomTile()));//32
-		fourSquares.add(new Square(5, 3, true, randomTile()));//33
-		fourSquares.add(new Square(5, 4, true, new LetterTile("U", 3)));//34
-		fourSquares.add(new Square(5, 5, true, new LetterTile("R", 2)));//35
-		
-		duneSquares.add(new Square(0, 0, true, randomTile()));//0
-		duneSquares.add(new Square(0, 1, true, randomTile()));//1
-		duneSquares.add(new Square(0, 2, true, new LetterTile("D", 3)));//2
-		duneSquares.add(new Square(0, 3, true, randomTile()));//3
-		duneSquares.add(new Square(0, 4, true, randomTile()));//4
-		duneSquares.add(new Square(0, 5, true, randomTile()));//5
-		duneSquares.add(new Square(1, 0, true, new LetterTile("U", 3)));//6
-		duneSquares.add(new Square(1, 1, true, new LetterTile("A", 2)));//7
-		duneSquares.add(new Square(1, 2, true, new LetterTile("D", 3)));//8
-		duneSquares.add(new Square(1, 3, true, new LetterTile("I", 2)));//9
-		duneSquares.add(new Square(1, 4, true, new LetterTile("B", 4)));//10
-		duneSquares.add(new Square(1, 5, true, randomTile()));//11
-		duneSquares.add(new Square(2, 0, true, new LetterTile("M", 3)));//12
-		duneSquares.add(new Square(2, 1, true, new LetterTile("S", 2)));//13
-		duneSquares.add(new Square(2, 2, true, new LetterTile("I", 2)));//14
-		duneSquares.add(new Square(2, 3, true, new LetterTile("K", 5)));//15
-		duneSquares.add(new Square(2, 4, true, new LetterTile("A", 2)));//16
-		duneSquares.add(new Square(2, 5, true, new LetterTile("R", 2)));//17
-		duneSquares.add(new Square(3, 0, true, new LetterTile("E", 1)));//18
-		duneSquares.add(new Square(3, 1, true, new LetterTile("L", 3)));//19
-		duneSquares.add(new Square(3, 2, true, new LetterTile("A", 2)));//20
-		duneSquares.add(new Square(3, 3, true, new LetterTile("G", 4)));//21
-		duneSquares.add(new Square(3, 4, true, new LetterTile("A", 2)));//22
-		duneSquares.add(new Square(3, 5, true, new LetterTile("R", 2)));//23
-		duneSquares.add(new Square(4, 0, true, new LetterTile("M", 3)));//24
-		duneSquares.add(new Square(4, 1, false,new BlankTile()));//25
-		duneSquares.add(new Square(4, 2, true, new LetterTile("N", 2)));//26
-		duneSquares.add(new Square(4, 3, false,new BlankTile()));//27
-		duneSquares.add(new Square(4, 4, true, new LetterTile("E", 1)));//28
-		duneSquares.add(new Square(4, 5, false,new BlankTile()));//29
-		duneSquares.add(new Square(5, 0, false,new BlankTile()));//30
-		duneSquares.add(new Square(5, 1, false,new BlankTile()));//31
-		duneSquares.add(new Square(5, 2, false,new BlankTile()));//32
-		duneSquares.add(new Square(5, 3, false,new BlankTile()));//33
-		duneSquares.add(new Square(5, 4, false,new BlankTile()));//34
-		duneSquares.add(new Square(5, 5, false,new BlankTile()));//35
-		
-		latinSquares.add(new Square(0, 0, true, new LetterTile("C", 3)));//0
-		latinSquares.add(new Square(0, 1, true, randomTile()));//1
-		latinSquares.add(new Square(0, 2, true, randomTile()));//2
-		latinSquares.add(new Square(0, 3, false,new BlankTile()));//3
-		latinSquares.add(new Square(0, 4, true, new LetterTile("O", 2)));//4
-		latinSquares.add(new Square(0, 5, true, new LetterTile("R", 2)));//5
-		latinSquares.add(new Square(1, 0, true, new LetterTile("C", 3)));//6
-		latinSquares.add(new Square(1, 1, true, new LetterTile("A", 2)));//7
-		latinSquares.add(new Square(1, 2, false,new BlankTile()));//8
-		latinSquares.add(new Square(1, 3, true, randomTile()));//9
-		latinSquares.add(new Square(1, 4, true, new LetterTile("H", 2)));//10
-		latinSquares.add(new Square(1, 5, true, new LetterTile("A", 2)));//11
-		latinSquares.add(new Square(2, 0, true, new LetterTile("A", 2)));//12
-		latinSquares.add(new Square(2, 1, true, randomTile()));//13
-		latinSquares.add(new Square(2, 2, true, new LetterTile("E", 1)));//14
-		latinSquares.add(new Square(2, 3, false,new BlankTile()));//15
-		latinSquares.add(new Square(2, 4, true, randomTile()));//16
-		latinSquares.add(new Square(2, 5, true, new LetterTile("C", 3)));//17
-		latinSquares.add(new Square(3, 0, true, new LetterTile("T", 1)));//18
-		latinSquares.add(new Square(3, 1, true, new LetterTile("S", 2)));//19
-		latinSquares.add(new Square(3, 2, false,new BlankTile()));//20
-		latinSquares.add(new Square(3, 3, true, new LetterTile("S", 2)));//21
-		latinSquares.add(new Square(3, 4, true, randomTile()));//22
-		latinSquares.add(new Square(3, 5, true, new LetterTile("E", 1)));//23
-		latinSquares.add(new Square(4, 0, true, new LetterTile("U", 3)));//24
-		latinSquares.add(new Square(4, 1, true, new LetterTile("U", 3)));//25
-		latinSquares.add(new Square(4, 2, true, randomTile()));//26
-		latinSquares.add(new Square(4, 3, false,new BlankTile()));//27
-		latinSquares.add(new Square(4, 4, true, new LetterTile("A", 2)));//28
-		latinSquares.add(new Square(4, 5, true, randomTile()));//29
-		latinSquares.add(new Square(5, 0, true, new LetterTile("L", 3)));//30
-		latinSquares.add(new Square(5, 1, true, new LetterTile("L", 3)));//31
-		latinSquares.add(new Square(5, 2, false,new BlankTile()));//32
-		latinSquares.add(new Square(5, 3, true, randomTile()));//33
-		latinSquares.add(new Square(5, 4, true, randomTile()));//34
-		latinSquares.add(new Square(5, 5, true, new LetterTile("R", 2)));//35
-		
-		Board food = new Board (foodSquares);
-		Board r = new Board (rSquares);
-		Board four = new Board (fourSquares);
-		Board dune = new Board (duneSquares);
-		Board latin = new Board (latinSquares);
-		Board[] themeBoards = {food, r, four, dune, latin};
-		String[] themeNames = {foodName, rName, fourName, duneName, latinName};
-		ThemeDictionary[] themeDicts = {foodDic, rDic, fourDic, duneDic, latinDic};
-		List<List<Tile>> themeTiles = new ArrayList<List<Tile>>(5);
 		int row, col;
 		for (int i = 0; i < 36; i++) {
 			row = (int) Math.floor(i/6);
 			col = i%6;
 			
-			for (int j = 0; j < 5; j++){
-				themeTiles.add(new ArrayList<Tile>());
-				themeTiles.get(j).add(themeBoards[j].getBoardSquares().get(i).getTile());
-			}
 			Tile tile = randomTile();
 			Square square = new Square(row, col, true, tile);
 			randomBoardSquares.add(i, square);
@@ -288,20 +55,13 @@ public class Model {
 
 		for (int i = 1; i <= 5; i++) {
 			this.loadIn("puzzle", i);
-			
-			//board.loadInBoard(squaresInPlay);
-			for (int j = 0; j < 36; j++) {
-				System.out.print(board.getBoardSquares().get(i).getSquareInPlay());
-			}
-			System.out.println("");
 			puzzleLevel[i] = new PuzzleLevel(starVal, squaresInPlay, board, i, numMoves);
+			
 			this.loadIn("lightning", i);
-			System.out.println("time:" + time);
 			lightningLevel[i] = new LightningLevel(starVal, squaresInPlay, board, i, time);
+			
 			this.loadIn("theme", i);
-			//themeWords = "SADNESS\nWORRY\nANXIETY";
 			ThemeDictionary themeDict = new ThemeDictionary(Arrays.asList(themeWords.split("\\n")));
-			//themeLevel[i] = new ThemeLevel(starVal, squaresInPlay, themeTiles.get(i-1), themeBoards[i-1], i, themeNames[i-1], themeDicts[i-1]);
 			themeLevel[i] = new ThemeLevel(starVal, squaresInPlay, null, board, i, themeName, themeDict);
 		}
 	}
@@ -427,10 +187,7 @@ public class Model {
 			return new BlankTile ();
 		}
 	}
-/**
- * get/set
- *
- */
+
 	public void loadIn(String levType, int levNum) {
 		ArrayList<String> contents = new ArrayList<String>();
 		String pathName = "src/levels/" + levType.toUpperCase() + levNum + ".txt";
@@ -483,7 +240,7 @@ public class Model {
 				allWords = allWords.concat("\n");
 			}
 			System.out.println("HERE" + allWords);
-			this.themeWords = allWords;
+			this.themeWords = allWords.toUpperCase();
 		}
 	}
 	
@@ -497,14 +254,6 @@ public class Model {
 	
 	public void setLevels(Level level[]) {
 		this.level = level;
-	}
-	
-	public LevelSelect getLevelSelect() {
-		return ls;
-	}
-	
-	public void setLevelSelect(LevelSelect ls) {
-		this.ls = ls;
 	}
 	
 	public PuzzleLevel getPuzzleLevel(int levNum) {
--- LetterCraze - Cadmium/src/game/model/ThemeLevel.java
+++ LetterCraze - Cadmium/src/game/model/ThemeLevel.java
@@ -111,30 +111,10 @@ public class ThemeLevel extends Level {
 		this.levelType = "theme";
 	}
 
-	public void findThemeWordPlacement () {
-		int numOfWords = this.themeWords.words.size();
-		ArrayList<ArrayList<String>> listOfThemeWordLetters = new ArrayList<ArrayList<String>>();
-		for (int p = 0; p < numOfWords; p++) {
-			listOfThemeWordLetters.add(parseThemeWords(this.themeWords.words.get(p)));
-		}
-
-		for (int p = 0; p < numOfWords; p++) {
-			for (int q = 0; q < listOfThemeWordLetters.get(p).size(); q++) {
-				int index = 0;
-				String letter = listOfThemeWordLetters.get(p).get(q);
-				LetterTile lt = new LetterTile(letter, this.board.getLetterScores().get(letter.toUpperCase()));
-				this.initBoardSquares.get(index).setTile(lt);
-				index++;
-			}
-		}
-
-		for (int n = 0; n < 36; n++) {
-			if (this.initBoardSquares.get(n).getTile() == null) {
-				this.initBoardSquares.get(n).setTile(randomTile());
-			}
-		}
-	}
-
+	/**
+	 *  Begin Theme Word Placement Code
+	 */
+	
 	public ArrayList<Square> generateThemeWordBoard() {
 		ArrayList<Square> tempBoardSquares = new ArrayList<Square>();
 		int row, col;
@@ -173,11 +153,11 @@ public class ThemeLevel extends Level {
 			System.out.println("The given list of words has two many letters to be placed on the board");
 			return -2;
 		}
-		if (numSqs == -1 && timesTried < 1) {
+		if (numSqs == -1 && timesTried < 5) {
 			timesTried++;
 			return themeWordsPlacement(wordList, tempBoard);
 		}
-		if (timesTried == 1) {
+		if (timesTried == 5) {
 			resetThemeWordBoard(tempBoard);
 			System.out.println("Function ran 5 times before failing");
 			return -1;
@@ -187,7 +167,6 @@ public class ThemeLevel extends Level {
 		return 1;
 	}
 
-	// the first word entered has a currentWord of 0
 	public int placeWordsRecursion(Board b, List<String> wordList, int currentWord) {
 		int numLet = 0;
 		int numAvail = 0;
@@ -348,239 +327,9 @@ public class ThemeLevel extends Level {
 		}
 	}
 
-
-	/*
-
-		for (int p = 0; p < listOfThemeWordLetters.size(); p++) {
-			boolean foundSequence = false;
-			int n;
-			while (!foundSequence) {
-				for (n = 0; n < 36; n++) {
-				if (recursionAlgorithm(this.beingUsed, listOfThemeWordLetters.get(p).size(), this.initBoardSquares.get(n))) {
-					foundSequence = true;
-			} else {
-				foundSequence = false;
-			}
-			}
-				if (!foundSequence) {
-					System.out.println("Could not generate Theme level with given board shape and Words");
-					break;
-				}
-			}
-		}
-		for (int p = 0; p < numOfWords; p++) {
-			for (int q = 0; q < listOfThemeWordLetters.get(p).size(); q++) {
-				int r = 0;
-				int index = this.initBoardSquares.indexOf(this.beingUsed.get(r));
-				String letter = listOfThemeWordLetters.get(p).get(q);
-				LetterTile lt = new LetterTile(letter, this.board.getLetterScores().get(letter.toUpperCase()));
-				this.initBoardSquares.get(index).setTile(lt);
-			}
-		}
-
-			for (int j = 0; j < 36; j++) {
-				if (this.board.boardSquares.get(j).getSquareInPlay()) {
-					if (this.board.boardSquares.get(j).getTile() instanceof BlankTile) {
-						this.board.boardSquares.get(j).setTile(this.randomTile());
-					}
-				}
-			}
-		}
-
-
-	public boolean recursionAlgorithm (ArrayList<Square> beingUsed, int numOfLetters, Square s) {
-		numOfLetters --;
-		beingUsed.add(s);
-		int squareNum = ((6*s.getSquareRow()) + s.getSquareColumn());
-		int neighbors [] = {-6, -5, 1, 7, 6, 5, -1, -7};
-
-		if (numOfLetters == 0 && checkValidStart(s, beingUsed)) {
-			return true;
-		}
-		if (!checkValidStart(s, beingUsed)) {
-			numOfLetters++;
-			beingUsed.remove(beingUsed.size() - 1);
-			return false;
-		}
-		boolean foundPath = false;
-		if (numOfLetters != 0 && squareNum == 0) {
-			for (int m = 2; m < 5; m++) {
-				if (checkValidStart(s, beingUsed) && recursionAlgorithm(beingUsed, numOfLetters, this.initBoardSquares.get(squareNum + neighbors[m]))) {
-					foundPath = true;
-					break;
-				} else {
-					foundPath = false;
-				}
-			} if (foundPath) {
-				return true;
-			} else {
-				numOfLetters++;
-				beingUsed.remove(beingUsed.size() - 1);
-				return false;
-			}
-			}
-
-		if (numOfLetters != 0 && squareNum == 5) {
-			for (int m = 4; m < 7; m++) {
-				if (checkValidStart(s, beingUsed) && recursionAlgorithm(beingUsed, numOfLetters, this.initBoardSquares.get(squareNum + neighbors[m]))) {
-					foundPath = true;
-					break;
-				} else {
-					foundPath = false;
-				}
-			} if (foundPath) {
-				return true;
-			} else {
-				numOfLetters++;
-				beingUsed.remove(beingUsed.size() - 1);
-				return false;
-			}
-			}
-
-		if (numOfLetters != 0 && squareNum == 35) {
-			for (int m = 0; m < 8; m++) {
-				if (m == 6 || m == 7 || m == 0) {
-					if (checkValidStart(s, beingUsed) && recursionAlgorithm(beingUsed, numOfLetters, this.initBoardSquares.get(squareNum + neighbors[m]))) {
-						foundPath = true;
-						break;
-					} else {
-						foundPath = false;
-					}
-				}
-				} if (foundPath) {
-					return true;
-				} else {
-					numOfLetters++;
-					beingUsed.remove(beingUsed.size() - 1);
-					return false;
-				}
-			}
-
-
-		if (numOfLetters != 0 && squareNum == 30) {
-			for (int m = 0; m < 3; m++) {
-				if (checkValidStart(s, beingUsed) && recursionAlgorithm(beingUsed, numOfLetters, this.initBoardSquares.get(squareNum + neighbors[m]))) {
-					foundPath = true;
-					break;
-				} else {
-					foundPath = false;
-				}
-			} if (foundPath) {
-				return true;
-			} else {
-				numOfLetters++;
-				beingUsed.remove(beingUsed.size() - 1);
-				return false;
-			}
-			}
-
-		if (numOfLetters != 0 && (s.getSquareColumn() == 0) && (squareNum != 0 && squareNum != 30)) {
-			for (int m = 0; m < 5; m++) {
-				if (checkValidStart(s, beingUsed) && recursionAlgorithm(beingUsed, numOfLetters, this.initBoardSquares.get(squareNum + neighbors[m]))) {
-					foundPath = true;
-					break;
-				} else {
-					foundPath = false;
-				}
-			} if (foundPath) {
-				return true;
-			} else {
-				numOfLetters++;
-				beingUsed.remove(beingUsed.size() - 1);
-				return false;
-			}
-			}
-
-		if (numOfLetters != 0 && (s.getSquareColumn() == 5) && (squareNum != 5 && squareNum != 35)) {
-			for (int m = 0; m < 8; m++) {
-				if (m == 0 || m >= 4) {
-				if (checkValidStart(s, beingUsed) && recursionAlgorithm(beingUsed, numOfLetters, this.initBoardSquares.get(squareNum + neighbors[m]))) {
-					foundPath = true;
-					break;
-				} else {
-					foundPath = false;
-				}
-				}
-			} if (foundPath) {
-				return true;
-			} else {
-				numOfLetters++;
-				beingUsed.remove(beingUsed.size() - 1);
-				return false;
-			}
-			}
-
-		if (numOfLetters != 0 && (s.getSquareRow() == 0) && (squareNum != 0 && squareNum != 5)) {
-			for (int m = 2; m < 7; m++) {
-				if (checkValidStart(s, beingUsed) && recursionAlgorithm(beingUsed, numOfLetters, this.initBoardSquares.get(squareNum + neighbors[m]))) {
-					foundPath = true;
-					break;
-				} else {
-					foundPath = false;
-				}
-			} if (foundPath) {
-				return true;
-			} else {
-				numOfLetters++;
-				beingUsed.remove(beingUsed.size() - 1);
-				return false;
-			}
-			}
-
-		if (numOfLetters != 0 && (s.getSquareRow() == 5) && (squareNum != 30 && squareNum != 35)) {
-			for (int m = 0; m < 8; m++) {
-				if (m <= 2 || m >= 6) {
-				if (checkValidStart(s, beingUsed) && recursionAlgorithm(beingUsed, numOfLetters, this.initBoardSquares.get(squareNum + neighbors[m]))) {
-					foundPath = true;
-					break;
-				} else {
-					foundPath = false;
-				}
-				}
-			} if (foundPath) {
-				return true;
-			} else {
-				numOfLetters++;
-				beingUsed.remove(beingUsed.size() - 1);
-				return false;
-			}
-			}
-
-		if (numOfLetters != 0 && s.getSquareRow() != 0 && s.getSquareColumn() !=0) {
-		for (int m = 0; m < 8; m++) {
-			if (checkValidStart(s, beingUsed) && recursionAlgorithm(beingUsed, numOfLetters, this.initBoardSquares.get(squareNum + neighbors[m]))) {
-				foundPath = true;
-				break;
-			} else {
-				foundPath = false;
-			}
-		} if (foundPath) {
-			return true;
-		} else {
-			numOfLetters++;
-			beingUsed.remove(beingUsed.size() - 1);
-			return false;
-		}
-		}
-
-		else {
-			numOfLetters++;
-			beingUsed.remove(beingUsed.size() - 1);
-			return false;
-		}
-	}		
+	/**
+	 *  End Theme Word Placement Code
 	 */
-	public ArrayList<String> parseThemeWords (String string) {
-		ArrayList<String> wordLetters = new ArrayList<String>();
-		for (int j = 0; j < string.length(); j++) {
-			wordLetters.add(Character.toString(string.charAt(j)));
-		}
-		return wordLetters;
-	}
-
-	public boolean checkValidStart(Square sqr) {
-		return sqr.getSquareInPlay();
-	}
 
 
 
